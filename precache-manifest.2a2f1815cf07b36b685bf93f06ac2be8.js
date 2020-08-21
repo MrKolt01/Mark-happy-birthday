1c10b7b65fb62d870988c2ab5e476c70f6b19ac7
mrkolt01 <kolt.zar@mail.ru>
2020-08-21 11:16:01 +0300

    Updates

---
+++ precache-manifest.2a2f1815cf07b36b685bf93f06ac2be8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c7c8c6e6de178bf90342716788413137",
+    "url": "/birthday-wisher/index.html"
+  },
+  {
+    "revision": "1cb558c0deb4c2e78709",
+    "url": "/birthday-wisher/static/css/main.1544317f.chunk.css"
+  },
+  {
+    "revision": "bb62dbc8bcd4548d6ff5",
+    "url": "/birthday-wisher/static/js/2.9bc39d80.chunk.js"
+  },
+  {
+    "revision": "fd228f9b9a68ce6b23a6cf8b0a38a0e3",
+    "url": "/birthday-wisher/static/js/2.9bc39d80.chunk.js.LICENSE"
+  },
+  {
+    "revision": "1cb558c0deb4c2e78709",
+    "url": "/birthday-wisher/static/js/main.551c3760.chunk.js"
+  },
+  {
+    "revision": "5b97457a1dabc9d16247",
+    "url": "/birthday-wisher/static/js/runtime-main.49da0469.js"
+  }
+]);
